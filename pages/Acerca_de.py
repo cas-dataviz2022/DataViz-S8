4a29be9f024f21f93e0196eee49d02cdbcab39fd
cas-dataviz2022 <115039776+cas-dataviz2022@users.noreply.github.com>
2022-11-23 21:00:56 -0300

    Update Acerca_de.py

--- pages/Acerca_de.py
+++ pages/Acerca_de.py
@@ -6,7 +6,7 @@ st.write("## Visualizaciones de Datos Geográficos en Internet")
 components.html("""
 <div style="background: white;border-radius: 5px;padding: 10px 25px;">
   <h3>
-    Creado para el curso de FAU
+    Creado para el curso de FAU - 2022
   </h3>
   <p>
     Creado por: <b>Cristian Acuña</b>
